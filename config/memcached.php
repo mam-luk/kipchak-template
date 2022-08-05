ec62604bdd654ed0d8d57f86b8503dba7b6789a6
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-08-05 10:36:55 +0400

    Add memcached dependency into container

--- config/memcached.php
+++ config/memcached.php
@@ -3,7 +3,7 @@
 use function Mamluk\Kipchak\env;
 
 return [
-    'default' => [
+    [
         'host' => env('MEMCACHED_HOST', 'memcached'),
         'port' => env('MEMCACHED_PORT', 11211),
     ]
