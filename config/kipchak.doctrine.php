db0cf1235f63eb1d7dc891677c9fe7c991f673b6
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-08-09 16:31:23 +0400

    Add doctrine dbal and entity managers via config

--- config/kipchak.doctrine.php
+++ config/kipchak.doctrine.php
@@ -4,7 +4,53 @@ use function Mamluk\Kipchak\env;
 
 return
 [
-    'dbal' => '',
+    'dbal' => [
+        'enabled' => true,
+        'connections' => [
+            // https://www.doctrine-project.org/projects/doctrine-dbal/en/current/reference/configuration.html
+                'primary' => [
+                    'database' => 'mydb',
+                    'username' => env('DB_USER', 'api'),
+                    'password' =>  env('DB_PASSWORD', 'api'),
+                    'host' => env('DB_HOST', 'mysql'),
+                    'port' => env('DB_PORT', 3306),
+                    'driver' => 'pdo_mysql',
+                    'charset' => 'utf-8'
+                ]
+            ]
+    ],
 
-    'orm' => ''
+    'orm' => [
+        'enabled' => true,
+        'entity_managers' => [
+            'primary' => [
+                // Enables or disables Doctrine metadata caching
+                // for either performance or convenience during development.
+                'dev_mode' => (bool) env('DEBUG', true),
+
+                // List of paths where Doctrine will search for metadata.
+                // Metadata can be either YML/XML files or PHP classes annotated
+                // with comments or PHP8 attributes (effectively Doctrine's database entities)
+                'metadata_dirs' => [
+                    realpath(__DIR__ . '/../database/Entities'),
+                    realpath(__DIR__ . '/../api/Entities')
+                ],
+                'connection' => 'primary', // Name of connection from 'dbal' above to use for the ORM
+                'cache' => [
+                    'enabled' => true, // Kipchak will only look at this if dev_mode = false
+                    'store' => 'memcached', // file or memcached.
+                ],
+                'cache_config' => [
+                    'memcached' => [
+                        // Pool where Doctrine will cache the processed metadata when 'dev_mode' is false
+                        'pool' => 'cache'
+                    ],
+                    'file' => [
+                        // Path where Doctrine will cache the processed metadata when 'dev_mode' is false
+                        'dir' => '/tmp/primary',
+                    ]
+                ]
+            ],
+        ]
+    ]
 ];
