2c06379f130e8d4226c8bdfb328da4ed997f6379
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-10-06 06:23:28 +0400

    Add preflight requests

--- html/index.php
+++ html/index.php
@@ -1,5 +1,12 @@
 <?php
+/**
+ * Tweak these headers as needed, especially if you are going to use this API from a client side SPA
+ * without a Backend for Frontend
+ */
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Authorization, origin');
 
 require_once (realpath(__DIR__ . '/../vendor/autoload.php'));
 
