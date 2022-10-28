0154c8c8e24a00c0a56fa266f61f02d498110a7d
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram / مِيزَانُ الْدِّينْ عَبْدُ ذِيْ الْجَلَالِ وَ الْإِكْرَامِ <m@meezaan.net>
2022-10-28 18:00:52 +0400

    Update kipchak.couchdb.php

--- config/kipchak.couchdb.php
+++ config/kipchak.couchdb.php
@@ -7,7 +7,7 @@ return [
     'connections' => [
         'default' => [
             'host' => env('COUCHDB_HOST', 'http://couchdb'), # No trailing slash, please.
-            'port' => getenv('COUCHDB_PORT') !== null ? (int) getenv('COUCHDB_PORT') : null,
+            'port' => getenv('COUCHDB_PORT') !== false ? (int) getenv('COUCHDB_PORT') : null,
             'username' => env('COUCHDB_USER', 'api'),
             'password' => env('COUCHDB_PASSWORD', 'api'),
             'database' => env('COUCHDB_DATABASE', 'api_database')
