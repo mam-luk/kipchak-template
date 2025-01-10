2c73f5c743891d32631428446d3202da9f141e9e
Meezaan-ud-Din A Wal-Ikram <m@meezaan.net>
2025-01-10 18:49:40 +0400

    Add exception handling config

--- config/kipchak.api.php
+++ config/kipchak.api.php
@@ -5,6 +5,7 @@ use function Mamluk\Kipchak\env;
 return [
     'name' => 'kipchak-template', // Hyphen or underscore separated string
     'debug' => (bool) env('DEBUG', true),
+    'logExceptionDetails' => true,
     // If debug is enabled, loglevel is debug. Otherwise, it is info. Overwrite it by specifying it below.
     // 'loglevel' => \Monolog\Level::Debug
 ];
